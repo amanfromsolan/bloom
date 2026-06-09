ddb938b7add3925bc8424b25a8bcf53d2dfcfbaf
Aman <aman@hey.com>
2026-06-09 17:13:42 +0800

    Initial Commit

---
+++ cmux-alternative/cmux_alternativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cmux_alternativeApp.swift
+//  cmux-alternative
+//
+//  Created by aman on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct cmux_alternativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cmux-alternativeTests/cmux_alternativeTests.swift
@@ -0,0 +1,19 @@
+//
+//  cmux_alternativeTests.swift
+//  cmux-alternativeTests
+//
+//  Created by aman on 09/06/26.
+//
+
+import Testing
+@testable import cmux_alternative
+
+struct cmux_alternativeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
